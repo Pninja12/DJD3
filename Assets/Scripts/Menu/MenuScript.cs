d500e8e0929ebceffd25f1dde4abd8a697b4b93a
JoaoCarvalho2002 <jonitkick@gmail.com>
2025-05-30 17:29:28 +0100

    ADD Multiple Textures, changed lights , created menu visuals and script

---
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
+
+    }
+    public void Options()
+    {
+
+
+
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+
+
+
+    }
+
+}
